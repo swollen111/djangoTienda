fc5db02959b2f9cfc723d897b07cabb61b9f8d08
swollen111 <beeyt22@gmail.com>
2024-11-19 23:48:26 +0100

    Add files via upload

---
+++ ProyectoWebApp/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render, HttpResponse
+
+from carro.carro import Carro
+
+
+
+# Create your views here.
+
+def home(request):
+
+   carro=Carro(request)
+   
+   return render(request, "ProyectoWebApp/home.html")
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
